61f0921f8015cd39a57243ef7ea73aa8895cd986
abart711 <bartlingalex@gmail.com>
2013-03-26 17:19:48 -0500

    IW changes

--- Iron_Worker/hello_worker.rb
+++ Iron_Worker/hello_worker.rb
@@ -2,5 +2,5 @@
 require 'rest_client'
 
 puts "Starting HelloWorker at #{Time.now}"
-RestClient.get "http://hidden-meadow-7494.herokuapp.com/workers/scrape_plates"
+puts RestClient.get "http://hidden-meadow-7494.herokuapp.com/workers/scrape_plates"
 puts "HelloWorker completed at #{Time.now}"
