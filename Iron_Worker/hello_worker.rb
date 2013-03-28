12785e5cc5d13c8d5bd75d71931f14f258f20ada
abart711 <bartlingalex@gmail.com>
2013-03-27 23:25:02 -0500

    add eviroments and iron_worker to ignore

--- Iron_Worker/hello_worker.rb
+++
@@ -1,6 +0,0 @@
-# Worker code can be anything you want.
-require 'rest-client'
-
-puts "Starting HelloWorker at #{Time.now}"
-puts RestClient.get "http://hidden-meadow-7494.herokuapp.com/workers/scrape_plates"
-puts "HelloWorker completed at #{Time.now}"
